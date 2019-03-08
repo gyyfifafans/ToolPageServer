b9c94ba8b1cd86a1cbfb6d3854b8f5137cec7281
Baka_kaku <gyyfifafans@126.com>
2019-03-08 14:42:16 +0800

    Update mac_server_.py
    后端分离官网和app

--- server/mac_server_.py
+++ server/mac_server_.py
@@ -100,6 +100,18 @@ def getAuthCode():
 
     return json.dumps(jsonObject,cls=MyEncoder)
 
+def getAuthCodeWeb():
+    jsonObject = {}
+    r=redis.Redis(host='172.30.73.10',port=6379,db=1,password='IeydzcujuhnI25yEdGUz5n14')
+    for key,value in keys.items():
+        try:
+            k=value.format(phone=request.args.get('phone'),process=key)
+            result=r.get(k)
+            jsonObject[content[key]] = result
+        except Exception as e:
+            print ("error message==>",e)
+
+    return json.dumps(jsonObject,cls=MyEncoder)
 
 @app.route("/getHkLv2",methods=["GET"])
 def getHKLv2():
